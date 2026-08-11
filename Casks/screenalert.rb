64b4a40cdae4e6326621a8d5b9106f80a81cc320
GUITTENY Brian <bguitteny@luccasoftware.com>
2026-08-11 18:55:43 +0200

    chore: bump screenalert cask to v0.5.6

--- Casks/screenalert.rb
+++ Casks/screenalert.rb
@@ -1,10 +1,10 @@
 cask "screenalert" do
-  version "0.5.1"
+  version "0.5.6"
   sha256 :no_check
 
   url "https://github.com/briangtn/ScreenAlerts/releases/download/v#{version}/ScreenAlert.zip"
   name "ScreenAlert"
-  desc "Menu bar app that displays full-screen alerts before calendar events"
+  desc "Menu bar app that displays full-screen overlay alerts before calendar events — even over full-screen apps"
   homepage "https://github.com/briangtn/ScreenAlerts"
 
   depends_on macos: ">= :sonoma"
